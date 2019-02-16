c569a5e4ba8b5f26357e18972dfdc0bdd98c4d07
a <a@g.com>
2019-02-16 10:40:16 +0530

    =date fixx

--- server/utils/message.js
+++ server/utils/message.js
@@ -4,14 +4,14 @@ var generateMessage = (from, text) => {
     return {
         from,
         text,
-        completedAt: moment().add(5,'h').add(30,'m').format('h:mm:ss a')
+        completedAt: moment().add(5,'h').add(30,'m').format('h:mm a')
     }
 }
 var generateLocationMessage = (from, latitude, longitude) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        completedAt: moment().add(5,'h').add(30,'m').format('h:mm:ss a')
+        completedAt: moment().add(5,'h').add(30,'m').format('h:mm a')
     }
 }
 
